7be136a844d7f6b7ed60bae959615c5f83c6ffc0
shaunweg <31806106+shaunweg@users.noreply.github.com>
2018-06-21 11:17:02 -0500

    Create welcome.js

---
+++ welcome.js
@@ -0,0 +1,4 @@
+
+var visitorName = prompt("Hello, what is your name~?");
+
+document.getElementById("visitorGreeting").innerHTML = "Hello " + visitorName + "~! <br> This is an example page using HTML, CSS, & JS"; 
